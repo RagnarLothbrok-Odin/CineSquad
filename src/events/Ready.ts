58e336f9c22567dc339bc7ee529495831d35bf76
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-02-01 12:59:07 +0000

    🐛 fix(Ready.ts): update permissions in invite link to fix incorrect permissions for bot

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -49,7 +49,7 @@ export class Ready {
         );
         console.log(
             'Invite:'.white.bold,
-            `https://discordapp.com/oauth2/authorize?client_id=${client.user?.id}&scope=bot%20applications.commands&permissions=535327927376`.blue.underline.bold,
+            `https://discordapp.com/oauth2/authorize?client_id=${client.user?.id}&scope=bot%20applications.commands&permissions=1093404061143`.blue.underline.bold,
         );
 
         // Bot Specs
