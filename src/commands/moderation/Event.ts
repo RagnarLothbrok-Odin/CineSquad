f60c3833ed558f47cc7bebf140405f491ce570c7
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-02-01 17:31:32 +0000

    🚀 feat(Event.ts): add slash command to configure the hosting module with the specified channel

---
+++ src/commands/moderation/Event.ts
@@ -0,0 +1,48 @@
+import { Discord, Slash, SlashOption } from 'discordx';
+import type { CommandInteraction, TextChannel } from 'discord.js';
+import { ApplicationCommandOptionType, PermissionsBitField } from 'discord.js';
+import { Category } from '@discordx/utilities';
+import { KeyvInstance } from '../../utils/Util.js';
+
+@Discord()
+@Category('Staff')
+export class Event {
+    /**
+     * Allows configuration of the hosting module
+     * @param channel - The channel host threads will start in
+     * @param interaction - The command interaction.
+     */
+    @Slash({
+        description: 'Configure the hosting module',
+        defaultMemberPermissions: [PermissionsBitField.Flags.ManageMessages],
+    })
+    async event(
+        @SlashOption({
+            description: 'The channel host threads will start in',
+            name: 'channel',
+            required: true,
+            type: ApplicationCommandOptionType.Channel,
+        })
+            channel: TextChannel,
+            interaction: CommandInteraction,
+    ) {
+        if (!interaction.channel) return;
+
+        // Check if the bot has the SendMessages permission in the specified channel
+        if (!channel.permissionsFor(channel.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+            return interaction.reply({
+                content: `I am missing the \`SendMessages\` permission in ${channel}.\nPlease grant me this permission and try running the command again.`,
+            });
+        }
+
+        // Set the hosting channel for the guild in the Keyv database
+        await KeyvInstance().set(interaction.guild!.id, {
+            hosting: channel.id,
+        });
+
+        // Send a confirmation message about the updated hosting channel
+        await interaction.reply({
+            content: `Host threads will now start in ${channel}.`,
+        });
+    }
+}
