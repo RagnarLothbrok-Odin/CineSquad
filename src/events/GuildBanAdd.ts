15faec4758bd2c41724042b52e807f0f1514c10c
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-02-05 16:19:02 +0000

    📦 chore(GuildBanAdd.ts): add GuildBanAdd event handler to handle the guildBanAdd event in Discord.js

---
+++ src/events/GuildBanAdd.ts
@@ -0,0 +1,53 @@
+import type { ArgsOf, Client } from 'discordx';
+import { Discord, Once } from 'discordx';
+import { ChannelType, EmbedBuilder, PermissionsBitField } from 'discord.js';
+import { color, deleteGuildProperty, KeyvInstance } from '../utils/Util.js';
+
+/**
+ * Discord.js GuildBanAdd event handler.
+ */
+@Discord()
+export class GuildBanAdd {
+    /**
+     * Executes when the GuildBanAdd event is emitted.
+     * @param ban
+     * @param client - The Discord client.
+     * @returns void
+     */
+    @Once({ event: 'guildBanAdd' })
+    async onGuildBanAdd([ban]: ArgsOf<'guildBanAdd'>, client: Client) {
+        // Retrieve data for the current guild from Keyv
+        const data = await KeyvInstance()
+            .get(ban.guild!.id);
+
+        // If logging is enabled, send to channel
+        if (data && data.eventLogging) {
+            // Fetch the logging channel
+            const channel = await client.channels.fetch(data.eventLogging);
+
+            // Check if the channel exists, is a text channel, and has the necessary permissions to send messages
+            if (channel && channel.type === ChannelType.GuildText
+                && channel.permissionsFor(channel.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+                // Create an embed with information about the banned member
+                const embed = new EmbedBuilder()
+                    .setColor(color(ban.guild.members.me!.displayHexColor))
+                    .setAuthor({ name: 'Member Banned', iconURL: ban.user.displayAvatarURL() })
+                    .setThumbnail(ban.user.displayAvatarURL())
+                    .setDescription(`${ban} ${ban.user.username}${ban.user.discriminator !== '0' ? `#${ban.user.discriminator}` : ''}`)
+                    .setFooter({ text: `ID: ${ban.user.id}` })
+                    .setTimestamp();
+
+                // Add a field for the ban reason if it exists
+                if (ban.reason) {
+                    embed.addFields({ name: 'Reason', value: `\`${ban.reason}\`` });
+                }
+
+                // Send the embed to the logging channel
+                if (channel) channel.send({ embeds: [embed] });
+            } else {
+                // If the channel doesn't exist or bot lacks SendMessages permission, remove 'welcome' property
+                await deleteGuildProperty(ban.guild.id, 'eventLogging');
+            }
+        }
+    }
+}
