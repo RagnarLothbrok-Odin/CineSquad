bb5fabfe6cd9907285707d95d0952a9bc93f6ac4
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-02-05 15:14:01 +0000

    ✨ feat(GuildMemberAdd.ts): add support for event logging to send a welcome message to a specified channel when a member joins the guild

--- src/events/GuildMemberAdd.ts
+++ src/events/GuildMemberAdd.ts
@@ -1,9 +1,11 @@
-import type { ArgsOf } from 'discordx';
+import type { ArgsOf, Client } from 'discordx';
 import { Discord, Once } from 'discordx';
-import { GuildTextBasedChannel, PermissionsBitField } from 'discord.js';
+import {
+    ChannelType, EmbedBuilder, GuildTextBasedChannel, PermissionsBitField,
+} from 'discord.js';
 import canvafy from 'canvafy';
 import ordinal from 'ordinal';
-import { deleteGuildProperty, KeyvInstance } from '../utils/Util.js';
+import { color, deleteGuildProperty, KeyvInstance } from '../utils/Util.js';
 
 /**
  * Discord.js GuildMemberAdd event handler.
@@ -17,7 +19,7 @@ export class GuildMemberAdd {
      * @returns void
      */
     @Once({ event: 'guildMemberAdd' })
-    async onGuildMemberAdd([member]: ArgsOf<'guildMemberAdd'>) {
+    async onGuildMemberAdd([member]: ArgsOf<'guildMemberAdd'>, client: Client) {
         // Retrieve data for the current guild from Keyv
         const data = await KeyvInstance()
             .get(member.guild!.id);
@@ -28,7 +30,8 @@ export class GuildMemberAdd {
             const channel = member.guild?.channels.cache.get(data.welcome) as GuildTextBasedChannel;
 
             // Check if the channel exists and the bot has SendMessages permission
-            if (channel && channel.permissionsFor(channel.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+            if (channel && channel.type === ChannelType.GuildText
+                && channel.permissionsFor(channel.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
                 const welcome = await new canvafy.WelcomeLeave()
                     .setAvatar(member.user.displayAvatarURL({
                         forceStatic: true,
@@ -69,6 +72,37 @@ export class GuildMemberAdd {
             } else {
                 // If the role doesn't exist or bot lacks ManageRoles permission, remove 'autorole' property
                 await deleteGuildProperty(member.guild.id, 'autorole');
+            }
+        }
+
+        // If logging is enabled, send to channel
+        if (data && data.eventLogging) {
+            const channel = await client.channels.fetch(data.eventLogging);
+
+            if (channel && channel.type === ChannelType.GuildText
+                && channel.permissionsFor(channel.guild.members.me!).has(PermissionsBitField.Flags.SendMessages)) {
+                if (member.partial) await member.fetch();
+
+                const embed = new EmbedBuilder()
+                    .setColor(color(member.guild.members.me!.displayHexColor))
+                    .setAuthor({
+                        name: `${member.guild.name}`,
+                        iconURL: `${member.user.displayAvatarURL()}`,
+                    })
+                    .setDescription(
+                        `**Member Joined:** ${member.user} - \`@${member.user.tag}\` - \`${member.user.id}\`\n**Account Created:** <t:${Math.round(
+                            member.user.createdTimestamp / 1000,
+                        )}> - (<t:${Math.round(member.user.createdTimestamp / 1000)}:R>)\n**Joined:** <t:${Math.round(
+                            member.joinedTimestamp! / 1000,
+                        )}> - (<t:${Math.round(member.joinedTimestamp! / 1000)}:R>)`,
+                    )
+                    .setFooter({ text: `ID: ${member.user.id}` })
+                    .setTimestamp();
+
+                if (channel) channel.send({ embeds: [embed] });
+            } else {
+                // If the channel doesn't exist or bot lacks SendMessages permission, remove 'welcome' property
+                await deleteGuildProperty(member.guild.id, 'eventLogging');
             }
         }
     }
